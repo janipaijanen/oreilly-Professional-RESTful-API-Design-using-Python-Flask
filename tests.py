64f1339979014d3412c8ea5fefa14d97c10bee56
Jani Päijänen <jani.paijanen@gmail.com>
2018-11-28 11:37:15 +0200

    token tests works

---
+++ tests.py
@@ -0,0 +1,12 @@
+# Set the path
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import unittest
+
+from app.tests import AppTest
+#from store.tests import StoreTest
+#from pet.tests import PetTest
+
+if __name__ == '__main__':
+    unittest.main()
