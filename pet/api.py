a0f7d5beacf18852f962853d3aebfc3684acde86
Jani Päijänen <jani.paijanen@gmail.com>
2018-11-28 09:07:37 +0200

    POST pet endpoint

--- pet/api.py
+++ pet/api.py
@@ -10,3 +10,13 @@ class PetAPI(MethodView):
 
     def get(self):
         return jsonify({"pets": self.pets})
+
+    def post(self):
+        if not request.json or not 'name' in request.json:
+            abort(400)
+        pet = {
+            'id': len(self.pets)+1,
+            'name': request.json['name']
+        }
+        self.pets.append(pet)
+        return jsonify({'pet': pet}), 201
